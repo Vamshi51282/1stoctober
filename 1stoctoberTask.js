f1cc192b481ff117d7baf9b9578416301679f25d
Vamshi51282 <135943611+Vamshi51282@users.noreply.github.com>
2024-11-21 17:10:02 +0530

    Add files via upload

---
+++ 1stoctoberTask.js
@@ -0,0 +1,118 @@
+var a="tuesday"
+switch(a){
+    case "sunday":
+        console.log("this is sunday");
+        break
+        case "monday":
+            console.log("this is monday");
+            break
+            case "tuesday":
+                console.log("this is tuesday");
+                break
+                case "wednesday":
+                    console.log("this is wednesday");
+                    break
+                    case "thrusday":
+                        console.log("this is thrusday");
+                        break
+                        case "friday":
+                            console.log("this is friday");
+                            break
+                            default:
+                                console.log("none of the day");
+                                
+        
+}
+
+
+var b="august"
+switch(b){
+    case "january":
+        console.log("sankrathi is special in januaury");
+        break
+        case"february":
+        console.log("in feb it consists leap year");
+        break
+        case"march":
+        console.log("its march");
+        break
+        case"april":
+        console.log("its april");
+        break
+        case"may":
+        console.log("its may");
+        break
+        case"june":
+        console.log("its june");
+        break
+        case"july":
+        console.log("its july");
+        break
+        case"august":
+        console.log("its august");
+        break
+        case"september":
+        console.log("its september");
+        break
+        case"october":
+        console.log("its october");
+        break
+        default:
+            console.log("none of the month");
+        
+}
+
+
+a=1
+while(a<=10){
+    if(a%2==0){
+        console.log("its even number");
+    }else{
+        console.log("its odd number");
+        
+    }
+    a++
+      
+}
+i=10
+while(i>=1){
+    console.log("the game starts in"+i);
+    i--
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
